c18236738f2a66e0925712517a7d669baf89cb78
Radek <interradek82@gmail.com>
2023-04-18 21:00:44 +0200

    Dodanie testów jednostkowych

--- src/test/ProductModelTest.java
+++ src/test/ProductModelTest.java
@@ -1,4 +1,37 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class ProductModelTest {
-  
+import org.junit.jupiter.api.Assertions;
+
+public class ProductModelTest {
+    ProductModel product = new ProductModel();
+    @Test //testuje poprawność obliczania całkowitej ceny produktu na podstawie ceny i ilości
+    public void testCalculateTotalPrice() {
+        product.setPrice(5.50);
+        product.setQuantity(4);
+
+        Assertions.assertEquals(22, product.calculateTotalPrice());
+    }
+
+    @Test // testuje poprawność pobierania nazwy produktu
+    public void testGetProductName() {
+        product.setProductName("Chleb");
+
+        Assertions.assertEquals("Chleb", product.getProductName());
+    }
+
+    @Test //testuje poprawność pobierania ceny produktu
+    public void testGetPrice() {
+        product.setPrice(5.50);
+
+        Assertions.assertEquals(5.50, product.getPrice());
+    }
+
+    @Test //testuje poprawność pobierania ilości produktu
+    public void testGetQuantity() {
+        product.setQuantity(4);
+
+        Assertions.assertEquals(4, product.getQuantity());
+    }
+
 }
